3d16a7b7b48e3df53be66a7446a5f1a730d7f1e7
0NightAlex0 <0887185@hr.nl>
2017-06-28 12:13:08 +0200

    drawl between multiple objects
    change position: size (amount of elements)
    edit code in Drawlines

--- Assets/DrawLine.cs
+++ Assets/DrawLine.cs
@@ -6,9 +6,9 @@ public class DrawLine : MonoBehaviour {
     private LineRenderer lineRenderer;
 
     public Transform origin;
+    public Transform checkpoint;
     public Transform destination;
-    //test beacon
-    //private Vector3 beacon = new Vector3(0.0f, 18.5f, 80f);
+
     // Use this for initialization
     void Start () {
         lineRenderer = GetComponent<LineRenderer>();
@@ -20,6 +20,7 @@ public class DrawLine : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         lineRenderer.SetPosition(0, origin.position);
-        lineRenderer.SetPosition(1, destination.position);
+        lineRenderer.SetPosition(1, checkpoint.position);
+        lineRenderer.SetPosition(2, destination.position);
     }
 }
