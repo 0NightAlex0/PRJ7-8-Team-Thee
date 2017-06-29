47a1249ac31d69ae890412707bae0fbfc6d13ef6
120944 <lucashbp@gmail.com>
2017-06-29 12:54:39 +0200

    changed drawline to accept a config field for LineHeight

---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+    private LineRenderer lineRenderer;
+
+    public float Stroke;
+    public float LineHeight;
+    public Transform origin;
+    public Transform checkpoint;
+    public Transform destination;
+
+    // Use this for initialization
+    void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = Stroke;
+        lineRenderer.endWidth = Stroke;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(0, getAdjustedVector(origin.position));
+        lineRenderer.SetPosition(1, getAdjustedVector(checkpoint.position));
+        lineRenderer.SetPosition(2, getAdjustedVector(destination.position));
+    }
+
+    Vector3 getAdjustedVector(Vector3 v) {
+        var newPos = v;
+        newPos.y = LineHeight;
+        return newPos;
+    }
+}
