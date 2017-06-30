8837651338a14ce511b789f78bb44a53eb6456d6
0NightAlex0 <0887185@hr.nl>
2017-06-30 13:53:56 +0200

    destroy route when checkpoint reached

--- Assets/DrawLine.cs
+++ Assets/DrawLine.cs
@@ -8,19 +8,33 @@ public class DrawLine : MonoBehaviour {
     public Transform origin;
     public Transform checkpoint;
     public Transform destination;
+    private List<Transform> listTransform;
 
     // Use this for initialization
     void Start () {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.startWidth = .45f;
         lineRenderer.endWidth = 0.45f;
-
+        //last in first out
+        listTransform = new List<Transform>(new Transform[] {  destination, checkpoint,  origin });
     }
 	
 	// Update is called once per frame
 	void Update () {
-        lineRenderer.SetPosition(0, origin.position);
-        lineRenderer.SetPosition(1, checkpoint.position);
-        lineRenderer.SetPosition(2, destination.position);
+
+
+        for(int i = 0; i < listTransform.Count;i++)
+        {
+            lineRenderer.SetPosition(i, listTransform[i].position);
+        }
+        if (listTransform[listTransform.Count-1].position == listTransform[listTransform.Count - 2].position && listTransform.Count >=2)
+        {
+            listTransform[listTransform.Count - 2] = listTransform[listTransform.Count - 1];
+            listTransform.RemoveAt(listTransform.Count - 1);
+            lineRenderer.SetVertexCount(lineRenderer.positionCount - 1);
+        }
+
+
+
     }
 }
