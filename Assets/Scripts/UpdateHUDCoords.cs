e8b55c9e04fdf3862de43b343eaf4e72a18c5896
0NightAlex0 <0887185@hr.nl>
2017-07-03 16:23:43 +0200

    semi working location

--- Assets/Scripts/UpdateHUDCoords.cs
+++ Assets/Scripts/UpdateHUDCoords.cs
@@ -1,20 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class UpdateHUDCoords : MonoBehaviour {
-    public GameObject GuiText;
-    public GameObject PlayerGameObject;
+//using Windows.Devices.Geolocation;
 
-    // Use this for initialization
-    void Start() {
+//using System;
 
-    }
+//public class UpdateHUDCoords : MonoBehaviour
+//{
+//    public GameObject GuiText;
+//    public GameObject PlayerGameObject;
+//    Text x;
+//    Location loc;
+//    // Use this for initialization
+//    void Start()
+//    {
+//        loc = new Location();
+//        x = GuiText.GetComponent<Text>();
+//    }
 
-    // Update is called once per frame
-    void Update() {
-        var x = GuiText.GetComponent<Text>();
-        x.text = PlayerGameObject.transform.position.ToString();
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        loc.setlonglat();
+//        if (loc.lati != null && loc.longi != null)
+//        {
+//            x.text = loc.longi.ToString() + loc.lati.ToString();
+//        }
+//        else
+//        {
+//            x.text = "no connection werkt helemaal niet";
+//        }
+
+//    }
+//    public class Location
+//    {
+//        public double lati;
+//        public double longi;
+//        public async void setlonglat()
+//        {
+//            Geolocator geolocator = new Geolocator();
+//            geolocator.DesiredAccuracyInMeters = 5;
+
+//            Geoposition position =
+//                await geolocator.GetGeopositionAsync(
+//                TimeSpan.FromMinutes(0),
+//                TimeSpan.FromSeconds(3));
+//            lati = position.Coordinate.Latitude;
+//            longi = position.Coordinate.Longitude;
+
+//        }
+
+//    }
+
+//}
+
